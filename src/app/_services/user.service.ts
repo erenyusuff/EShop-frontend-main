a23c113c492edaedf50b16c07a0c2697eaf5b205
Yusuf Eren <yusuf.er48en@gmail.com>
2023-08-02 16:51:12 +0300

    EShop Frontend main - registration,login and get profile with token

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from "../../environments/environment";
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getPublicContent(): Observable<any> {
+    return this.http.get(environment.api + '/user', { responseType: 'text' });
+  }
+
+  getUserBoard(): Observable<any> {
+    return this.http.get(environment.api + '/auth/profile', { responseType: 'text' });
+  }
+
+  getModeratorBoard(): Observable<any> {
+    return this.http.get(environment.api + '/mod', { responseType: 'text' });
+  }
+
+  getAdminBoard(): Observable<any> {
+    return this.http.get(environment.api + '/admin', { responseType: 'text' });
+  }
+}
